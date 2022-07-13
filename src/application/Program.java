875af6e5e000ad1b86dde93c2e1cff90e1495002
Darwin <joaodarwin.ip22@gmail.com>
2022-07-13 13:12:40 -0300

    Movendo peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,29 @@
 package application;
 
-import boardgame.BoardException;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		try {
-			ChessMatch chessMatch = new ChessMatch();
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while (true) {
 			UI.printBoard(chessMatch.getPieces());
-		}
-		catch (BoardException e) {
-			System.out.print(e.getMessage());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 		}
 	}
 
